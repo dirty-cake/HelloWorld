8f379352ab3bca03cd30ac76dfca867f3b7aedad
zadira <zadirakh@gmail.com>
2019-11-27 20:20:10 +0200

    conditions

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 new Vue ({
     el: '#app',
     data: {
-        value: 1
+        show: false,
+        cars: [
+            {model:"BMW", speed: 250.8},
+            {model:"Audi", speed: 240.21},
+            {model:"Mercedes-Benz AMG", speed: 350.4},
+            {model:"Ford", speed: 160.5}
+        ]
+            
+
+        
+        // 1-2 lessons
+        // value: 1
         // title: "Hello World!",
         // styleCSS: ''
     },
     methods: {
-        increment(value) {
-            this.value = value
-            if(value == 25)
-            alert("Number 25!")
-        }
+        // 3 lesson
+        // increment(value) {
+        //     this.value = value
+        //     if(value == 25)
+        //     alert("Number 25!")
+        // }
         // changeText() {
         //     this.title = 'Any new text'
         // }
     },
     computed: {
-        doubleValue() {
-            return this.value * 2
-        }
+        // doubleValue() {
+        //     return this.value * 2
+        // }
     }
 });
