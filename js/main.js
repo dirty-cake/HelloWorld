0866cd93fde180a2afd438a5268a596a899f2c96
zadira <zadirakh@gmail.com>
2019-11-27 14:24:02 +0200

    method changeText

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ new Vue ({
     el: '#app',
     data: {
         title: "Hello World!"
+    },
+    methods: {
+        changeText() {
+            this.title = 'Any new text'
+        }
     }
 });
