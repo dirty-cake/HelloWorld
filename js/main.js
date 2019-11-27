2f76ed372f5134a4dadc09b959eee8b74a1f1f49
zadira <zadirakh@gmail.com>
2019-11-27 20:47:00 +0200

    work with filters

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
+Vue.filter ('capatilize', function (value){
+    if (!value) return ''
+    value = value.toString()
+    return value.replace(/\b\w/g, function(l) {return l.toUpperCase() })
+});
 new Vue ({
     el: '#app',
     data: {
         show: false,
+        message: 'Hello, I am Ira. I am twenty one yeras old. I want to be a programmer', 
         cars: [
             {model:"BMW", speed: 250.8},
             {model:"Audi", speed: 240.21},
             {model:"Mercedes-Benz AMG", speed: 350.4},
             {model:"Ford", speed: 160.5}
         ]
-            
-
         
         // 1-2 lessons
         // value: 1
@@ -28,8 +32,16 @@ new Vue ({
         // }
     },
     computed: {
+        showMess() {
+            return this.message.toUpperCase ()
+        }
         // doubleValue() {
         //     return this.value * 2
         // }
+    },
+    filters: {
+        lowercase (value) {
+            return value.toLowerCase ()
+        }
     }
 });
