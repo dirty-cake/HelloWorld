bbfad7572b2b1936bcce15639470ea77eec2b61d
zadira <zadirakh@gmail.com>
2019-11-27 17:42:53 +0200

    processed properties

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 new Vue ({
     el: '#app',
     data: {
-        title: "Hello World!",
-        styleCSS: ''
+        value: 1
+        // title: "Hello World!",
+        // styleCSS: ''
     },
     methods: {
-        changeText() {
-            this.title = 'Any new text'
+        increment(value) {
+            this.value = value
+            if(value == 25)
+            alert("Number 25!")
+        }
+        // changeText() {
+        //     this.title = 'Any new text'
+        // }
+    },
+    computed: {
+        doubleValue() {
+            return this.value * 2
         }
     }
 });
